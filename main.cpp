15f041c2386ae3af20937ea5ddb9ffc5787e4c0b
Lucas Nogueira <lucasnogueiratdm@hotmail.com>
2018-07-13 17:04:59 -0300

    testando alocação dinamica

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define BRASIL		setlocale(LC_ALL, "portuguese_brazil")
+
+//DEFININDO ESTRUTURA ALUNO
+struct ALUNO{
+	unsigned short codigo;
+	char nome[30];
+	unsigned short nota;
+};
+//DECLARANDO ESTRUTURA
+struct ALUNO *aluno;
+
+int main() {	
+	printf("Largura da estrutura: %d bytes\n", sizeof(ALUNO));
+	aluno = (ALUNO*)calloc(5,sizeof(ALUNO));
+	
+	for(int i=0; i<5; i++){
+		printf("Aluno %d: codigo-> %d | nome-> %s | nota-> %d\n", i, aluno[i].codigo, aluno[i].nome, aluno[i].nota);
+	}
+	
+	return 0;
+}
