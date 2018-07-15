009d00f341bd4371b4a9f6d37a5826478b6a0ca9
Lucas Nogueira <lucasnogueiratdm@hotmail.com>
2018-07-15 18:22:36 -0300

    função de cadastro completa

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <locale.h>
 
-#define BRASIL		setlocale(LC_ALL, "portuguese_brazil")
+#define BRASIL			setlocale(LC_ALL, "portuguese_brazil")
+#define LIMPAR_TELA		system("cls")	
 
 //DEFININDO ESTRUTURA ALUNO
 struct ALUNO{
@@ -13,13 +14,48 @@ struct ALUNO{
 //DECLARANDO ESTRUTURA
 struct ALUNO *aluno;
 
-int main() {	
-	printf("Largura da estrutura: %d bytes\n", sizeof(ALUNO));
-	aluno = (ALUNO*)calloc(,sizeof(ALUNO));
-	
-	for(int i=0; i<5; i++){
-		printf("Aluno %d: codigo-> %d | nome-> %s | nota-> %d\n", i, aluno[i].codigo, aluno[i].nome, aluno[i].nota);
-	}
-	
+//PROTOTIPOS
+void cadastrar_ALUNOS();
+void pesquisar_ALUNOS();
+void excluir_ALUNOS();
+void alterar_ALUNOS();
+void mostrar_ALUNOS();
+
+//Entry of the main
+int main() {BRASIL;
+	cadastrar_ALUNOS();
+	mostrar_ALUNOS();
 	return 0;
+}
+
+//PROCEDIMENTO: cadastrar Aluno
+void cadastrar_ALUNOS(){	
+	unsigned short qtde = 0;//var recebe quantidade
+	
+	printf("Insira a quantidade de alunos[números]:"); scanf("%d", &qtde);
+	
+	aluno = (ALUNO*)calloc(qtde, sizeof(ALUNO));//alocando memoria
+	
+	//loop de criação de alunos
+	for(int i = 0; i < qtde; i++){
+		printf("Matricula:"); 	scanf("%d", &aluno[i].codigo);
+		printf("Nome:"); 		scanf("%s", &aluno[i].nome);
+		printf("Nota:"); 		scanf("%d", &aluno[i].nota);
+		
+		if(i != qtde-1)//chegou no ultimo indice? 
+			++aluno;//proximo aluno
+			
+		LIMPAR_TELA;
+	}
+} 
+
+//PROCEDIMENTO: mostrar Aluno
+void mostrar_ALUNOS(){
+	int i=0; //variavel genérica
+	printf("id |	matricula    nome     nota\n");
+	while(aluno != NULL){
+		printf("%d | [%d] [%s] [%d]\n", i, aluno[i].codigo, aluno[i].nome, aluno[i].nota);
+		i++;
+		++aluno;
+	}
 }
